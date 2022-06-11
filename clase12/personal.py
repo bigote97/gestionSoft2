31a300d59f439139bf7c246aa5310d7e9b7acf77
augusto <augusto.rl@heybtech.com>
2022-06-10 21:14:50 -0300

    Inicializo personal con datos

--- clase12/personal.py
+++ clase12/personal.py
@@ -1,25 +1,29 @@
 from random import randint
+import permanentes from empleadoPermanente
+import eventuales from empleadoEventual
 import names
-empleados = []
-
-for i in range(10):
-    temp = {
-        'nombre': names.get_first_name(),
-        'apellido': names.get_last_name(),
-        'dni': randint(10000000, 99999999),
-        'salario': randint(10000, 99999),
-    }
-    aux = randint(0, 1)
-    if (aux == 1):
-        auxVentas = randint(1, 99)
-        temp['ventas'] = []
-        for i in range(auxVentas):
-            temp['ventas'].append(randint(1000, 9999))
-            
-    else:
-        temp['antiguedad'] = randint(0, 9)
-    empleados.append(temp)
 
 class personal:
     def __init__(self):
-        
+        empleados = []
+        for i in range(10):
+            temp = {
+                'nombre': names.get_first_name(),
+                'apellido': names.get_last_name(),
+                'dni': randint(10000000, 99999999),
+                'salario': randint(10000, 99999),
+            }
+            aux = randint(0, 1)
+            if (aux == 1):
+                auxVentas = randint(1, 99)
+                temp['ventas'] = []
+                for i in range(auxVentas):
+                    temp['ventas'].append(randint(1000, 9999))
+                    
+            else:
+                temp['antiguedad'] = randint(0, 9)
+            
+            empleados.append(temp)
+        self.empleados = empleados
+
+    def nuevoEmpleado(self, nombre, apellido, dni, salario, extra):
